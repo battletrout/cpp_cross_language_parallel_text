ea0e6dcf24fbcadbbf38379c51b2ff627a1b9044
Battletrout <nathanmkemp@gmail.com>
2024-11-11 12:30:32 +0000

    add part D to main

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,7 @@
 // #include "fileParser.hpp"
 #include "tokenizer.hpp"
 #include "textUtils.hpp"
+#include "pmiAggregator.hpp"
 //Private includes
 //#include "core/socketServer.cpp"
 //#include "core/async.cpp"
@@ -33,12 +34,13 @@ int main() {
     Tokenizer tok_grk(greek_locale);
     Tokenizer tok_eng(eng_locale);
 
+    PmiAggregator aggregator;
     // Nested map[english][greek] = count
-    std::unordered_map<std::string, std::unordered_map<std::string, uint32_t>> eng_to_grk_words;
-    // Map to store count of docs with english words
-    std::unordered_map<std::string, uint32_t> eng_words;
-    // Map to store count of docs with greek words
-    std::unordered_map<std::string, uint32_t> grk_words;   
+    // std::unordered_map<std::string, std::unordered_map<std::string, uint32_t>> eng_to_grk_words;
+    // // Map to store count of docs with english words
+    // std::unordered_map<std::string, uint32_t> eng_words;
+    // // Map to store count of docs with greek words
+    // std::unordered_map<std::string, uint32_t> grk_words;   
     auto text_pairs = ParallelTextParser::parse_file(greek_filepath);
 
     size_t line_id{0};
@@ -46,64 +48,50 @@ int main() {
         if (greek.empty() || english.empty()) continue;
         
         //For each line
-        line_id++;
+        aggregator.total_doc_count++;
         //generate sets of tokens (unique vals only)
         auto eng_tokens = tok_eng.tokenize(english);
         auto grk_tokens = tok_grk.tokenize(greek);
         
         bool first_grk_iteration = true;
         for (auto& eng_token : eng_tokens) {
-            eng_words.emplace(eng_token,0);
-            eng_words[eng_token]++;
+            aggregator.eng_words.emplace(eng_token,0);
+            aggregator.eng_words[eng_token]++;
             for (auto& grk_token : grk_tokens) {
-                eng_to_grk_words[eng_token].emplace(grk_token, 0);
-                eng_to_grk_words[eng_token][grk_token]++;
+                aggregator.eng_to_grk_words[eng_token].emplace(grk_token, 0);
+                aggregator.eng_to_grk_words[eng_token][grk_token]++;
                 
                 //if it's the first iteration of greek words, emplace them (only do it once)
                 if (first_grk_iteration) {
-                    grk_words.emplace(grk_token,0);
-                    grk_words[grk_token]++;
+                    aggregator.grk_words.emplace(grk_token,0);
+                    aggregator.grk_words[grk_token]++;
                 }
             }
             first_grk_iteration = false;
         }
-        if (line_id % 1000 == 0) {
-            debug_custom::log(debug_custom::LogLevel::INFO, fmt::format("processed line: {}",line_id));
+        if (aggregator.total_doc_count % 1000 == 0) {
+            debug_custom::log(debug_custom::LogLevel::INFO, fmt::format("processed line: {}",aggregator.total_doc_count));
         }
     }
         
     // for (const auto& [grk_word, count] : eng_to_grk_words["the"]) {
     //     std::cout << grk_word << ": " << count << std::endl;
     // }
+    std::cout << aggregator.eng_to_grk_words["airplane"]["αεροπλάνο"] <<std::endl;
+    std::cout << aggregator.grk_words["αεροπλάνο"] <<std::endl;
+    std::cout << aggregator.eng_words["airplane"] <<std::endl;
+    //PART C: Calculate for Airplane
+    auto [A, B, C, pmi] = aggregator.calc_PMI("airplane","αεροπλάνο");
+    fmt::print("For airplane to αεροπλάνο\nA: {}, B: {}, C: {}, PMI: {}\n\n", A, B, C, pmi);
 
-    //Calculate for Airplane
-    int A = eng_to_grk_words["airplane"]["αεροπλάνο"];
-    int B = eng_words["airplane"];
-    int C = grk_words["αεροπλάνο"];
-    int N = line_id;
-    float pmi = std::log2((N * A) /((A+B)*(A+C)));
-    fmt::print("For airplane to αεροπλάνο\nA: {}, B: {}, C: {}, PMI: {}", A, B, C, pmi);
+    //PART D: Calculate candidates for english words 
+    for (const auto& eng_word : {"telephone","swimming","helicopter","washington","bulgarian"}) {
+        std::cout << eng_word << " candidates: \n";
+        auto _ = aggregator.rank_translations(eng_word, 5, true);
+        std::cout << "\n\n";
+    }
 
-    A = eng_to_grk_words["bulgarian"]["βούλγαρος"];
-    B = eng_words["bulgarian"];
-    C = grk_words["βούλγαρος"];
-    N = line_id;
-    pmi = std::log2((N * A) /((A+B)*(A+C)));
-    fmt::print("For bulgarian to βούλγαρος\nA: {}, B: {}, C: {}, PMI: {}", A, B, C, pmi);
-    //return std::tuple{A,B,C,pmi};
-
-    A = eng_to_grk_words["bulgarian"]["βουλγαρικού"];
-    B = eng_words["bulgarian"];
-    C = grk_words["βουλγαρικού"];
-    N = line_id;
-    pmi = std::log2((N * A) /((A+B)*(A+C)));
-    fmt::print("For bulgarian to βουλγαρικού\nA: {}, B: {}, C: {}, PMI: {}", A, B, C, pmi);
-
-        // print_coll(grk_tokens);
-        // std::cout << std::endl;
-        // print_coll(eng_tokens);
-
-        //iterate through the vectors
-
+    //PART E: Calculate top-1 for English words that occur in 90 - 100 sentences
+    
     return 0;
 }
