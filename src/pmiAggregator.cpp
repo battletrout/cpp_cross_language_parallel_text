7015fde0a2469c5370105a0a12fcb35aaee83e64
Battletrout <nathanmkemp@gmail.com>
2024-11-11 12:30:19 +0000

    add pmiAggregator rank_translations. Part D complete, start Part E

--- src/pmiAggregator.cpp
+++ src/pmiAggregator.cpp
@@ -1,10 +1,11 @@
 #include <unordered_map>
 #include <string>
 #include <math.h>
+#include <fmt/core.h>
 
 #include "pmiAggregator.hpp"
 
-std::tuple<int,int,int,float> PmiAggregator::PmiVals(const std::string& eng_word, const std::string& grk_word) {
+std::tuple<int,int,int,float> PmiAggregator::calc_PMI(const std::string& eng_word, const std::string& grk_word) {
     int A = eng_to_grk_words[eng_word][grk_word];
     int B = eng_words[eng_word] - A;
     int C = grk_words[grk_word] - A;
@@ -12,7 +13,38 @@ std::tuple<int,int,int,float> PmiAggregator::PmiVals(const std::string& eng_word
     int N = total_doc_count;
 
     float pmi = std::log2((N * A) /((A+B)*(A+C)));
+    //fmt::print("For {} to {}\nA: {}, B: {}, C: {}, PMI: {}\n",eng_word,grk_word, A, B, C, pmi);
     return std::tuple{A,B,C,pmi};
-
 }
+
+std::vector<std::tuple<std::string, int, int, int, float>> PmiAggregator::rank_translations(
+    const std::string& eng_word_for_translation, int return_ranks, bool print) {
+        std::vector<std::tuple<std::string, int, int, int, float>> rankings;
+        //for each greek word that has appeared opposite the english word
+       for (const auto& [grk_word, count] : eng_to_grk_words[eng_word_for_translation]) {
+            auto [a,b,c,pmi] = calc_PMI(eng_word_for_translation, grk_word);
+            //remove a <= 5
+            if (a > 5) {
+                rankings.push_back({grk_word, a, b, c, pmi});
+            }
+       }
+
+       // Sort by PMI (last element of tuple) in descending order
+       std::sort(rankings.begin(), rankings.end(),
+            [](const auto& a, const auto& b) {
+                return std::get<4>(a) > std::get<4>(b); // > for descending order on pmi
+            });
+
+        // Print top 5 (or less if vector is smaller)
+        const size_t n = std::min(size_t(return_ranks), rankings.size());
+        for(size_t i = 0; i < n; i++) {
+            const auto& [word, a, b, c, pmi] = rankings[i];
+            if (print) {
+                fmt::print("#{}: {}, A: {}, B: {}, C: {}, PMI: {:.4f})\n", i+1, word, a, b, c, pmi);
+            }
+        }
+
+       return rankings;
+}
+
     
--- src/pmiAggregator.hpp
+++ src/pmiAggregator.hpp
@@ -11,9 +11,12 @@ struct PmiAggregator {
     // Map to store count of docs with greek words (C = this - A in table)
     std::unordered_map<std::string, uint32_t> grk_words;   
 
-    std::tuple<int,int,int,float> PmiVals(const std::string& eng_word, const std::string& grk_word);
+    std::tuple<int,int,int,float> calc_PMI(const std::string& eng_word, const std::string& grk_word);
+    
+    std::vector<std::tuple<std::string, int, int, int, float>> rank_translations(
+        const std::string& eng_word_for_translation, int return_ranks, bool print);   
     
     //D = this - (A + B + C) 
-    uint32_t total_doc_count;
+    uint32_t total_doc_count{0};
 
 };
